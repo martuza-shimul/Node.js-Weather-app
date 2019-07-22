24ab6035a4fea699533ff12c3f04c73b939a4363
Martuza Shimul <martuza.shimul@gmail.com>
2019-07-23 00:52:50 +0600

    es6 shorthand and destructuring initialize

--- utils/forecast.js
+++ utils/forecast.js
@@ -6,16 +6,16 @@ const forecast = (lat, lng, callback) => {
 
     const url = 'https://api.darksky.net/forecast/f3f71b0caf605df5e8188ad56cb5face/'+ lat +','+ lng +'?units=si'
 
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if(error){
             callback('Unable to connect to weather service!', undefined)
-        }else if(response.body.error){
+        }else if(body.error){
             callback('Unable to find the Location!', undefined)
         }else{
             callback(undefined, {
-                temperature:  response.body.currently.temperature,
-                chanceOfRain: response.body.currently.precipProbability + '%',
-                Humidity: response.body.currently.humidity 
+                temperature:  body.currently.temperature,
+                chanceOfRain: body.currently.precipProbability + '%',
+                Humidity: body.currently.humidity 
             })
         }
     })
--- utils/geocode.js
+++ utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoibWFydHV6YS1zaGltdWwiLCJhIjoiY2p3dGR2Ymp4MGkzaTN5bXFtcmx6NzRuMCJ9.IVmVLX5aqs21doCD-Hb-rg&limit=1'
 
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if(error){
             callback('Unable to connect to location Services!', undefined)
-        }else if(response.body.features.length === 0){
+        }else if(body.features.length === 0){
             callback('Unable to find location. Try another search', undefined)
         }else{
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
 
         }
